b481931e944c0efa560edc7fca4d0b2d9936c503
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-15 09:41:03 -0700

    jsjs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,10 @@
  * description : This is a c program that prints something using puts function
  *
  * return : 0 (success)
- */
+*/
 
 int main(void)
 {
-	puts"\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
