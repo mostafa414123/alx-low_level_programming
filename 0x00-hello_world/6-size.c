ae54b7589ea9efd73fca5543eeb04e776daaa0ff
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-15 13:11:29 -0700

    slsls

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,15 @@
  */
 int main(void)
 {
-	printf("Size of char is %zu\n", sizeof(char));
-	printf("Size of int is %zu\n", sizeof(int));
-	printf("Size of long int is %zu\n", sizeof(long int));
-	printf("Size of long long int is %zu\n", sizeof(long long int));
-	printf("Size of float is %zu\n", sizeof(float));
+	printf("Size of a char: %zu\n", sizeof(char));
+
+	printf("Size of an int: %zu\n", sizeof(int));
+
+	printf("Size of a long int: %zu\n", sizeof(long int));
+
+	printf("Size of a long long int: %zu\n", sizeof(long long int));
+
+	printf("Size of a float: %zu\n", sizeof(float));
 
 	return (0);
 }
