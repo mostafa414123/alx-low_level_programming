6f3a6c60ce36ed9a3630e4f120c28c4b95e0581a
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-15 12:51:58 -0700

    wpxk

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Auth: Mostafa Khaled
+ */
+
+#include <stdio.h>
+
+/**
+ * main - the output is  "with proper grammar, but the outcome is
+ *                a piece of art,"
+ *
+ * Return: should be  0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
