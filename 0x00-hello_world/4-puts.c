5aa46bdf2a7b4868756041bfe9a63621f43debcb
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-15 09:25:22 -0700

    cece

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main function : entry point
+ *
+ * description : This is a c program that prints something using puts function
+ *
+ * return : 0 (success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
