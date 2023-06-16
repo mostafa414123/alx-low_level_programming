190a0bf23c6c1a0cfb76bafd316fcbb7a87ed68c
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-16 16:43:10 -0700

    sos

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - Entry function for program
+  *
+  * Description : Prints numbers with commas and space
+  *
+  * Return: Always 0 (Success);
+  */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+
+		if (n == '9')
+		{
+			continue;
+		}
+		putchar(', ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
