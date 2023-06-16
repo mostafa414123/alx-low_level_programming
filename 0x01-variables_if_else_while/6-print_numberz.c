d9d54c73f290644f5d5cf95c11bccb19de572e3d
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-16 16:01:49 -0700

    qpxk

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Entry function for program
+  *
+  * Description : Prints the numbers from 0 to 9
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+        int number;
+
+        for (number = '0'; number <= '9'; number++)
+        {
+                putchar(number);
+        }
+
+        putchar('\n');
+
+        return (0);
+}
