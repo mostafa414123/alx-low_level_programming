ed243e9ee814831457a434f07df243044d49d6eb
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-20 08:24:52 -0700

    meme

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,22 @@
-_putchar
+#include "holberton.h"
+
+/**
+  * main - Entry point
+  *
+  * Description:  prints _putchar, followed by a new line
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	_putchar('M');
+	_putchar('O');
+	_putchar('S');
+	_putchar('T');
+	_putchar('A');
+	_putchar('F');
+	_putchar('A');
+	_putchar('\n');
+
+	return (0);
+}
