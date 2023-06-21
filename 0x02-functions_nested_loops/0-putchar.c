28610310b3db65ab92c223840a9ad59490d86d25
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-21 04:04:01 -0700

    eopx

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,13 +10,14 @@
 
 int main(void)
 {
-	char arr[] = "_putchar";
-	char i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(arr[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
