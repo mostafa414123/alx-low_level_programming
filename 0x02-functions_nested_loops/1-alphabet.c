6288cd60c077de997eccaee7df65488bf79d8afc
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-21 04:27:01 -0700

    elkmd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: call the function print_alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+    char i;
+    for (i = '97'; i < '123'; i++)
+	    _putchar(i);
+    _putchar('\n');
+}
