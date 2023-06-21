6f9ad6c66ffcb22759d4a8ec232e67e1bb12b32b
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-21 02:55:42 -0700

    wpap

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,18 +3,19 @@
 /**
  * main - Entry point
  *
- * Description: use function _putchar to print my name
+ * Description: use function _putchar to print _putchar
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	char arr[] = "Mostafa";
+	char arr[] = "_putchar";
 	char i;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(arr[i]);
 	}
+	_putchar('\n');
 }
