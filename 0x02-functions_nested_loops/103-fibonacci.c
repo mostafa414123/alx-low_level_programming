9c4f1187db4fd0f25c9b26f4b3cb2dbb78c10274
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-22 05:33:31 -0700

    wwwe

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,17 +15,16 @@ int main(void)
 	while (1)
 	{
 		sum = fibonacci_1 + fibonacci_2;
-		while (sum <= 4000000)
+		if (sum <= 4000000)
 		{
 			if ((sum % 2) == 0)
 				result += sum;
 			fibonacci_1 = fibonacci_2;
 			fibonacci_2 = sum;
 		}
-		if (sum <= 4000000)
-			printf("%.0f\n", result)
 		else
 			break;
 	}
+	printf("%.0f\n", result);
 	return (0);
 }
