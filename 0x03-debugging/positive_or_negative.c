a652376b9194fe5cb0fb6915928abea6b54c79a7
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-22 13:58:49 -0700

    ewet

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,16 +2,15 @@
 /**
  * positive_or_negative - function used to check number is
  * positive, negative or zero
- * @n: the input number that need to check
+ * @i: the input number that need to check
  * Return: Always 0 (success)
  */
-int positive_or_negative(int n)
+void positive_or_negative(int i)
 {
-	if (n == 0)
-		printf("%d is zero\n", n);
+	if (i == 0)
+		printf("%d is zero\n", i);
 	else if (n > 0)
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	else
-		printf("%d is negative\n", n);
-	return (0);
+		printf("%d is negative\n", i);
 }
