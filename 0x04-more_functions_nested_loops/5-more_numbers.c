5f0ff49c6567437609df3128ea11c32258c2163a
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-23 09:30:45 -0700

    qpod

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,8 +10,8 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
-			_putchar(j + '0');
+		for (j = 48; j <= 62; j++)
+			_putchar(j);
 		_putchar('\n');
 	}
 	_putchar('\n');
