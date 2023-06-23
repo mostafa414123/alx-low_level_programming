4359018d61088aba8d8486922c98b3933dac9fe7
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-23 08:59:42 -0700

    wmwm

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints the numbers, from
+ * 0 to 9 followed by a new line
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+}
