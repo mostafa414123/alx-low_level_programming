8af1e17643d543d8c073dc9c7406db589fcd7078
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-26 13:38:23 -0700

    rtwwd

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: the input pointer to char
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		count++;
+	}
+	while (count >= 0)
+	{
+		printf("%c", str[count]);
+		count--;
+	}
+	printf("\n");
+}
