97abbd96ae1bd8823b791f66b2237cc739676743
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-26 15:05:34 -0700

    ujsls

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character
+ * @str: the iput pointer to char
+ */
+void puts2(char *str)
+{
+	int count;
+
+	for (count = 0; count != '\0'; count++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[count]);
+	}
+	_putchar('\n');
+}
