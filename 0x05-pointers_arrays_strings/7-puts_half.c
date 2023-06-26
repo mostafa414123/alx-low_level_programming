61bf2747e0c2b161d1f76a2672dda17eb03fa320
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-26 15:20:30 -0700

    qwpo

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,7 +7,7 @@ void puts_half(char *str)
 {
 	int count;
 
-	for (count = 0; count != '\0'; count++)
+	for (count = 0; str[count] != '\0'; count++)
 		;
 	count++;
 	for (count /= 2; str[count] != '\0'; count++)
