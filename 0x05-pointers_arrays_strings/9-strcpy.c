7660d85d2c4c4d9907dbe4b4be3fa6c5d7864871
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-27 14:56:29 -0700

    qqwew

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcpy - function that copies the string pointed by src
+ * to the buffer pointed to dest
+ * @dest: buffer to copy the string
+ * @src: the source string that copied
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count;
+
+	while (src[count])
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	return (dest);
+}
