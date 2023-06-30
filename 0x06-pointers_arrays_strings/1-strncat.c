d2bd930e1c1d1c5eab3271dbbe9a030507a05658
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-30 01:14:26 -0700

    ertt

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strncat -  function that concatenates two strings
+ * @dest: the distination string
+ * @src: the source string
+ * @n: number of bytes
+ * Return: pointer oints to destination string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0, index = 0;
+
+	while (dest[count++])
+		index++;
+	for (count = 0; src[count] && count < n; count++)
+		dest[index++] = src[index];
+	return (dest);
+}
