17f4f6b5cf7ff8645b4810ada47c92692e212eac
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-30 01:05:23 -0700

    ewrt

--- 0-strcat.c
+++ 0-strcat.c
@@ -5,4 +5,13 @@
  * @src: resulting string to append
  * Return:  The pointer destination string @dest
  */
+char *_strcat(char *dest, char *src)
+{
+	int count = 0, index = 0;
 
+	while (dest[count++])
+		index++;
+	for (count = 0; src[count]; count++)
+		dest[index++] = src[count];
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * *_strcat - function that concatenates two strings
- * @dest: destination location
- * @src: reservation location
- * Return: pointer to the resulting string dest
+ * @dest: resulting string to return
+ * @src: resulting string to append
+ * Return:  The pointer destination string @dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int count1 = 0, count2 = 0;
+	int count = 0, index = 0;
 
-	while (dest[count1++])
-		;
-	for (count2 = 0; src[count2]; count2++)
-		dest[count1] = src[count2];
+	while (dest[count++])
+		index++;
+	for (count = 0; src[count]; count++)
+		dest[index++] = src[count];
 	return (dest);
 }
