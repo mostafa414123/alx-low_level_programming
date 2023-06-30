e8c97d88e06c64ea813d12865e95f82bbe2bc99b
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-30 15:25:40 -0700

    psos

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 
 	while (str[count])
 	{
-		while !(str[count] >= 'a' && str[count] <= 'z')
+		while (!(str[count] >= 'a' && str[count] <= 'z'))
 			count++;
 
 		if (str[count - 1] == ' ' ||
