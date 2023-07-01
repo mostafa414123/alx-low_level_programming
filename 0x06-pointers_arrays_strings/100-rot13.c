da180a70c9a339f5db562c2334490d75173becf4
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-06-30 19:10:34 -0700

    osis

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * *rot13 - function that encodes a string using rot13
+ * @str: the encoded string
+ * Return: pointer to encoded string
+ */
+char *rot13(char *str)
+{
+	int count = 0, index;
+	char alpha[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rotkey[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+	while (str[count])
+	{
+		for (index = 0; index < 52; index++)
+		{
+			if (str[count] == alpha[index])
+			{
+				str[count] = rotkey[index];
+				break;
+			}
+		}
+		count++;
+	}
+	return (str);
+}
