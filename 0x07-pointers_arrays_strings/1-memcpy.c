518b5fd021226a088ee948443f029571ac60822e
mostafa414123 <mostafakhaled1111999@gmail.com>
2023-07-03 07:40:41 -0700

    poeie

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,6 +11,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int count;
 
 	for (count = 0; count < n; count++)
-		dest[index] = src[index];
+		dest[count] = src[count];
 	return (dest);
 }
